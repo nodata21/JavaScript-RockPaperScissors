46ddd3746eea614becd3c7fc6c30e1acc0464530
nodata21 <82617372+nodata21@users.noreply.github.com>
2021-05-02 20:32:07 +0200

    Add files via upload

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -6,24 +6,17 @@ var scissors = document.getElementById("scissors");
 
 var output = document.getElementById("output");
 var ai = document.getElementById("ai");
-var zahl = document.getElementById("zahl");
 
 var text = ''; var cpu = '';
 
-const beats = [];
-beats['rock'] = 'scissors';
-beats['paper'] = 'rock';
-beats['scissors'] = 'paper';
+const beats = {'rock' : 'scissors', 'paper': 'rock', 'scissors': 'paper'};
 
 const hand = ['rock', 'paper', 'scissors'];
 
 function play() {
     
     cpu = Math.floor(Math.random() * 3);
-
     input.value = text = this.id;
-
-    zahl.textContent = cpu;
     ai.textContent = hand[cpu];
     
     if (hand[cpu] == beats[text]) {
