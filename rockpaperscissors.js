e6cbfe929102a62839e11fa3e2a8139fbf92392d
nodata21 <82617372+nodata21@users.noreply.github.com>
2021-05-02 16:09:26 +0200

    Add files via upload

---
+++ rockpaperscissors.js
@@ -0,0 +1,47 @@
+var input = document.getElementById("input");
+var button = document.getElementById("button");
+var output = document.getElementById("output");
+var ai = document.getElementById("ai");
+
+const beats = [];
+beats['rock'] = 'scissors';
+beats['paper'] = 'rock';
+beats['scissors'] = 'paper';
+
+const hand = ['rock', 'paper', 'scissors'];
+
+function play() {
+
+    input.value = text = this.id;
+    //input.value = text;
+    
+    z = Math.floor(Math.random() * (10 - 1));
+    if (z <= 3) {
+        z = 0;
+    }
+    else if ( z >= 3 && z <= 6) {
+        z = 1;
+    }
+    else if ( z > 6 ) {
+        z = 2;
+    }
+    cpu = z;
+
+    zahl.textContent = cpu;
+    ai.textContent = hand[cpu];
+    
+    if (hand[cpu] == beats[text]) {
+        output.textContent = 'Won!';
+    }
+    else if (hand[cpu] == text)
+    {
+        output.textContent = "Draw!";
+    }
+    else { 
+        output.textContent = "Lose!";
+    }
+}
+
+rock.addEventListener("click", play)
+paper.addEventListener("click", play)
+scissors.addEventListener("click", play)
